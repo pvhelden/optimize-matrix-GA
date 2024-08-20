7f8ffbf61960278b47bbbb1dbc4298965f2ae2da
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2024-08-20 14:41:49 +0200

    added matrices

---
+++ data/matrices/All_concatenated_motifs_trimmed-info_0.1.tf
@@ -0,0 +1,511 @@
+AC  cluster_3_node_1_2motifs
+XX
+ID  cluster_3_node_1_2motifs
+XX
+DE  csscsCCgsCCCCGCCCCsbCcccss
+P0           a         c         g         t
+1          162       678       296       168
+2          131       464       548       161
+3          372      1942       999       376
+4          310      2075       874       430
+5          187      2009      1151       342
+6          180      2795       456       258
+7          332      2583       460       314
+8          303       798      2340       248
+9          190      1418      1872       209
+10          73      3203       251       162
+11         137      2986       399       167
+12         116      3268       208        97
+13         117      3165       300       107
+14         137       191      3181       180
+15          85      3247       264        93
+16          60      3296       217       116
+17          98      3058       354       179
+18         188      2978       305       218
+19         190      2011      1168       320
+20         323      1470       960       936
+21         215      2569       633       272
+22         273      2261       570       585
+23         327      2146       794       422
+24         294      2048       902       445
+25         258      1003       752       372
+26         274       988       779       344
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: cluster_3_node_1_2motifs
+CC  AC: cluster_3_node_1_2motifs
+CC  id: cluster_3_node_1_2motifs
+CC  name: cluster_3_node_1_2motifs
+CC  version: 
+CC  name: cluster_3_node_1_2motifs
+CC  description: csscsCCgsCCCCGCCCCsbCcccss
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+XX
+//
+AC  cluster_2
+XX
+ID  cluster_2
+XX
+DE  cscAGCCAATCAgvg
+P0           a         c         g         t
+1          149      1123       326       260
+2         1435        96       227       100
+3          157       104      1471       126
+4           47      1700        64        47
+5           42      1718        55        43
+6         1676        76        49        57
+7         1576       131       110        41
+8           48       125        71      1614
+9          121      1394       282        61
+10        1334       168       284        72
+11         241       332      1113       172
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 2
+CC  accession: cluster_2
+CC  AC: cluster_2
+CC  id: cluster_2
+CC  name: cluster_2
+CC  version: 
+CC  name: cluster_2
+CC  description: cscAGCCAATCAgvg
+CC  transfac_consensus: 
+CC  matrix.nb: 2
+CC  consensus.strict: cAGCCAATCAg
+CC  consensus.strict.rc: CTGATTGGCTG
+CC  consensus.IUPAC: cAGCCAATCAg
+CC  consensus.IUPAC.rc: CTGATTGGCTG
+CC  consensus.regexp: cAGCCAATCAg
+CC  consensus.regexp.rc: CTGATTGGCTG
+XX
+//
+AC  cluster_1_node_2_3motifs
+XX
+ID  cluster_1_node_2_3motifs
+XX
+DE  ssvvsbsvcgyCACTTCCGGtyss--------
+P0           a         c         g         t
+1          727      2072      1014       516
+2          561      1079      2091       598
+3          415      1164      1069      1681
+4          240      3384       455       250
+5         3704       145       348       132
+6           27      3940       142       220
+7          125        88       111      4005
+8           61        61        77      4130
+9           34      4218        34        43
+10          40      4199        36        54
+11          81       139      3708       401
+12         104       607      3378       240
+13         266      1018       762      2283
+14         522      1136      1068      1603
+15         587      1602      1152       988
+16         566      1788      1278       697
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 3
+CC  accession: cluster_1_node_2_3motifs
+CC  AC: cluster_1_node_2_3motifs
+CC  id: cluster_1_node_2_3motifs
+CC  name: cluster_1_node_2_3motifs
+CC  version: 
+CC  name: cluster_1_node_2_3motifs
+CC  description: ssvvsbsvcgyCACTTCCGGtyss--------
+CC  transfac_consensus: 
+CC  matrix.nb: 3
+CC  consensus.strict: cgtCACTTCCGGttcc
+CC  consensus.strict.rc: GGAACCGGAAGTGACG
+CC  consensus.IUPAC: cgyCACTTCCGGtyss
+CC  consensus.IUPAC.rc: SSRACCGGAAGTGRCG
+CC  consensus.regexp: cg[ct]CACTTCCGGt[ct][cg][cg]
+CC  consensus.regexp.rc: [CG][CG][AG]ACCGGAAGTG[AG]CG
+XX
+//
+AC  cluster_1_node_1_2motifs
+XX
+ID  cluster_1_node_1_2motifs
+XX
+DE  sssssbsvcsbCACTTCCGGtbss--------
+P0           a         c         g         t
+1          571      1491       802       417
+2          448       862      1495       476
+3          317       924       831      1209
+4          176      2589       333       183
+5         2799       109       271       102
+6           17      2997       108       159
+7           97        64        81      3039
+8           46        46        51      3138
+9           25      3202        24        30
+10          30      3189        26        36
+11          59       109      2809       304
+12          76       445      2591       169
+13         217       795       596      1673
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 4
+CC  accession: cluster_1_node_1_2motifs
+CC  AC: cluster_1_node_1_2motifs
+CC  id: cluster_1_node_1_2motifs
+CC  name: cluster_1_node_1_2motifs
+CC  version: 
+CC  name: cluster_1_node_1_2motifs
+CC  description: sssssbsvcsbCACTTCCGGtbss--------
+CC  transfac_consensus: 
+CC  matrix.nb: 4
+CC  consensus.strict: cgtCACTTCCGGt
+CC  consensus.strict.rc: ACCGGAAGTGACG
+CC  consensus.IUPAC: csbCACTTCCGGt
+CC  consensus.IUPAC.rc: ACCGGAAGTGVSG
+CC  consensus.regexp: c[cg][cgt]CACTTCCGGt
+CC  consensus.regexp.rc: ACCGGAAGTG[ACG][CG]G
+XX
+//
+AC  cluster_1_node_3_2motifs
+XX
+ID  cluster_1_node_3_2motifs
+XX
+DE  -ssssbsvcsyCACTTCCGscssss-------
+P0           a         c         g         t
+1          510      1559       795       395
+2          417       869      1522       451
+3          288       973       784      1214
+4          140      2737       225       157
+5         2735       155       256       113
+6           18      3010       111       120
+7           86       126       109      2938
+8           48        87       114      3010
+9           35      3132        50        42
+10          41      3140        51        27
+11          54       126      2897       182
+12          61      1268      1783       147
+13         210      1910       511       628
+14         405      1122      1063       669
+15         397      1319       955       588
+16         457      1310      1017       475
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 5
+CC  accession: cluster_1_node_3_2motifs
+CC  AC: cluster_1_node_3_2motifs
+CC  id: cluster_1_node_3_2motifs
+CC  name: cluster_1_node_3_2motifs
+CC  version: 
+CC  name: cluster_1_node_3_2motifs
+CC  description: -ssssbsvcsyCACTTCCGscssss-------
+CC  transfac_consensus: 
+CC  matrix.nb: 5
+CC  consensus.strict: cgtCACTTCCGgcccc
+CC  consensus.strict.rc: GGGGCCGGAAGTGACG
+CC  consensus.IUPAC: csyCACTTCCGscsss
+CC  consensus.IUPAC.rc: SSSGSCGGAAGTGRSG
+CC  consensus.regexp: c[cg][ct]CACTTCCG[cg]c[cg][cg][cg]
+CC  consensus.regexp.rc: [CG][CG][CG]G[CG]CGGAAGTG[AG][CG]G
+XX
+//
+AC  cluster_3
+XX
+ID  cluster_3
+XX
+DE  csscsCCgsCCCCGCCCCscccccss
+P0           a         c         g         t
+1          162       678       296       168
+2          131       464       548       161
+3          372      1942       999       376
+4          310      2075       874       430
+5          187      2009      1151       342
+6          180      2795       456       258
+7          332      2583       460       314
+8          303       798      2340       248
+9          383      2029      2555       466
+10         208      4094       646       485
+11         166      4589       441       237
+12         143      4849       290       151
+13         192      4643       445       153
+14         179       210      4798       246
+15         103      4893       326       111
+16          88      4932       270       143
+17         117      4708       405       203
+18         224      4597       355       257
+19         236      2101      2631       465
+20         343      3045      1073       972
+21         424      3453      1080       476
+22         443      3168      1004       818
+23         327      2146       794       422
+24         294      2048       902       445
+25         258      1003       752       372
+26         274       988       779       344
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 6
+CC  accession: cluster_3
+CC  AC: cluster_3
+CC  id: cluster_3
+CC  name: cluster_3
+CC  version: 
+CC  name: cluster_3
+CC  description: csscsCCgsCCCCGCCCCscccccss
+CC  transfac_consensus: 
+CC  matrix.nb: 6
+XX
+//
+AC  cluster_1_node_6_3motifs
+XX
+ID  cluster_1_node_6_3motifs
+XX
+DE  -----ssscsyCACTTCCGGybs---------
+P0           a         c         g         t
+1          601      1630      1414      2559
+2          331      4806       645       422
+3         5266       234       471       233
+4           47      5708       176       273
+5          170       153       161      5720
+6           63       128       126      5887
+7           73      5980        80        71
+8           63      5996        63        82
+9          139       266      4393      1406
+10         235       843      4763       363
+11         555      1905      1303      2441
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 7
+CC  accession: cluster_1_node_6_3motifs
+CC  AC: cluster_1_node_6_3motifs
+CC  id: cluster_1_node_6_3motifs
+CC  name: cluster_1_node_6_3motifs
+CC  version: 
+CC  name: cluster_1_node_6_3motifs
+CC  description: -----ssscsyCACTTCCGGybs---------
+CC  transfac_consensus: 
+CC  matrix.nb: 7
+CC  consensus.strict: tCACTTCCGGt
+CC  consensus.strict.rc: ACCGGAAGTGA
+CC  consensus.IUPAC: yCACTTCCGGy
+CC  consensus.IUPAC.rc: RCCGGAAGTGR
+CC  consensus.regexp: [ct]CACTTCCGG[ct]
+CC  consensus.regexp.rc: [AG]CCGGAAGTG[AG]
+XX
+//
+AC  cluster_1_node_4_2motifs
+XX
+ID  cluster_1_node_4_2motifs
+XX
+DE  --------ssyCACTTCCkGyss---------
+P0           a         c         g         t
+1          410      1031       896      1775
+2          179      3337       352       244
+3         3573       150       248       141
+4           35      3778       122       177
+5          116       114       110      3772
+6           42        90        83      3897
+7           59      3939        66        48
+8           42      3973        41        56
+9          106       207      2571      1228
+10         184       576      3083       269
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 8
+CC  accession: cluster_1_node_4_2motifs
+CC  AC: cluster_1_node_4_2motifs
+CC  id: cluster_1_node_4_2motifs
+CC  name: cluster_1_node_4_2motifs
+CC  version: 
+CC  name: cluster_1_node_4_2motifs
+CC  description: --------ssyCACTTCCkGyss---------
+CC  transfac_consensus: 
+CC  matrix.nb: 8
+CC  consensus.strict: tCACTTCCgG
+CC  consensus.strict.rc: CCGGAAGTGA
+CC  consensus.IUPAC: yCACTTCCkG
+CC  consensus.IUPAC.rc: CMGGAAGTGR
+CC  consensus.regexp: [ct]CACTTCC[gt]G
+CC  consensus.regexp.rc: C[AC]GGAAGTG[AG]
+XX
+//
+AC  cluster_1_node_5_5motifs
+XX
+ID  cluster_1_node_5_5motifs
+XX
+DE  sssssbsvcsyCACTTCCGGybsss-------
+P0           a         c         g         t
+1         1237      3631      1809       911
+2          978      1948      3613      1049
+3          703      2137      1853      2895
+4          380      6121       680       407
+5         6439       300       604       245
+6           45      6950       253       340
+7          211       214       220      6943
+8          109       148       191      7140
+9           69      7350        84        85
+10          81      7339        87        81
+11         135       265      6605       583
+12         165      1875      5161       387
+13         476      2928      1273      2911
+14         927      2258      2131      2272
+15         984      2921      2107      1576
+16        1023      3098      2295      1172
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 9
+CC  accession: cluster_1_node_5_5motifs
+CC  AC: cluster_1_node_5_5motifs
+CC  id: cluster_1_node_5_5motifs
+CC  name: cluster_1_node_5_5motifs
+CC  version: 
+CC  name: cluster_1_node_5_5motifs
+CC  description: sssssbsvcsyCACTTCCGGybsss-------
+CC  transfac_consensus: 
+CC  matrix.nb: 9
+CC  consensus.strict: cgtCACTTCCGGctcc
+CC  consensus.strict.rc: GGAGCCGGAAGTGACG
+CC  consensus.IUPAC: csyCACTTCCGGybss
+CC  consensus.IUPAC.rc: SSVRCCGGAAGTGRSG
+CC  consensus.regexp: c[cg][ct]CACTTCCGG[ct][cgt][cg][cg]
+CC  consensus.regexp.rc: [CG][CG][ACG][AG]CCGGAAGTG[AG][CG]G
+XX
+//
+AC  cluster_1_node_7_8motifs
+XX
+ID  cluster_1_node_7_8motifs
+XX
+DE  sssssbsvcsyCACTTCCGGybsss-------
+P0           a         c         g         t
+1         2362      6349      3308      1773
+2         1814      3775      6146      2057
+3         1304      3767      3267      5454
+4          711     10927      1325       829
+5        11705       534      1075       478
+6           92     12658       429       613
+7          381       367       381     12663
+8          172       276       317     13027
+9          142     13330       164       156
+10         144     13335       150       163
+11         274       531     10998      1989
+12         400      2718      9924       750
+13        1031      4833      2576      5352
+14        1880      4215      3868      3829
+15        1564      4375      3196      2495
+16        1023      3098      2295      1172
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 10
+CC  accession: cluster_1_node_7_8motifs
+CC  AC: cluster_1_node_7_8motifs
+CC  id: cluster_1_node_7_8motifs
+CC  name: cluster_1_node_7_8motifs
+CC  version: 
+CC  name: cluster_1_node_7_8motifs
+CC  description: sssssbsvcsyCACTTCCGGybsss-------
+CC  transfac_consensus: 
+CC  matrix.nb: 10
+CC  consensus.strict: cgtCACTTCCGGtccc
+CC  consensus.strict.rc: GGGACCGGAAGTGACG
+CC  consensus.IUPAC: csyCACTTCCGGybss
+CC  consensus.IUPAC.rc: SSVRCCGGAAGTGRSG
+CC  consensus.regexp: c[cg][ct]CACTTCCGG[ct][cgt][cg][cg]
+CC  consensus.regexp.rc: [CG][CG][ACG][AG]CCGGAAGTG[AG][CG]G
+XX
+//
+AC  cluster_1_node_8_9motifs
+XX
+ID  cluster_1_node_8_9motifs
+XX
+DE  sssssssvcsyCACTTCCGGybsss-------
+P0           a         c         g         t
+1         2377      6380      3321      2092
+2         1826      3795      6172      2377
+3         1310      4114      3280      5466
+4          726     11273      1333       838
+5        11752       545      1374       499
+6           98     12936       497       639
+7          391       411       394     12974
+8          179       315       341     13335
+9          148     13663       187       172
+10         155     13657       177       181
+11         293       559     11292      2026
+12         410      2793     10193       774
+13        1051      4924      2648      5547
+14        1923      4326      3964      3957
+15        1608      4524      3307      2569
+16        1060      3252      2421      1233
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 11
+CC  accession: cluster_1_node_8_9motifs
+CC  AC: cluster_1_node_8_9motifs
+CC  id: cluster_1_node_8_9motifs
+CC  name: cluster_1_node_8_9motifs
+CC  version: 
+CC  name: cluster_1_node_8_9motifs
+CC  description: sssssssvcsyCACTTCCGGybsss-------
+CC  transfac_consensus: 
+CC  matrix.nb: 11
+CC  consensus.strict: cgtCACTTCCGGtccc
+CC  consensus.strict.rc: GGGACCGGAAGTGACG
+CC  consensus.IUPAC: csyCACTTCCGGybss
+CC  consensus.IUPAC.rc: SSVRCCGGAAGTGRSG
+CC  consensus.regexp: c[cg][ct]CACTTCCGG[ct][cgt][cg][cg]
+CC  consensus.regexp.rc: [CG][CG][ACG][AG]CCGGAAGTG[AG][CG]G
+XX
+//
+AC  cluster_1
+XX
+ID  cluster_1
+XX
+DE  sssssssvcsyCACTTCCGGybssscGGaasc
+P0           a         c         g         t
+1         2402      6457      3384      2122
+2         1849      3856      6259      2401
+3         1332      4168      3326      5539
+4          734     11431      1353       847
+5        11903       559      1395       508
+6           99     13116       504       646
+7          395       418       401     13151
+8          180       329       348     13508
+9          151     13846       196       172
+10         158     13846       179       182
+11         298       569     11459      2039
+12         415      2805     10365       780
+13        1067      5029      2695      5574
+14        1950      4375      4060      3980
+15        1626      4625      3348      2604
+16        1083      3302      2523      1253
+17         532      1149      1233       540
+18          12       119        47        17
+19          15        25       144        11
+20          14        26       141        14
+21          99        36        47        13
+22         121        30        31        13
+XX
+CC  residue_type: 
+CC  program: transfac
+CC  matrix.nb: 12
+CC  accession: cluster_1
+CC  AC: cluster_1
+CC  id: cluster_1
+CC  name: cluster_1
+CC  version: 
+CC  name: cluster_1
+CC  description: sssssssvcsyCACTTCCGGybssscGGaasc
+CC  transfac_consensus: 
+CC  matrix.nb: 12
+CC  consensus.strict: cgtCACTTCCGGtcccgcGGaa
+CC  consensus.strict.rc: TTCCGCGGGACCGGAAGTGACG
+CC  consensus.IUPAC: csyCACTTCCGGybssscGGaa
+CC  consensus.IUPAC.rc: TTCCGSSSVRCCGGAAGTGRSG
+CC  consensus.regexp: c[cg][ct]CACTTCCGG[ct][cgt][cg][cg][cg]cGGaa
+CC  consensus.regexp.rc: TTCCG[CG][CG][CG][ACG][AG]CCGGAAGTG[AG][CG]G
+XX
+//
